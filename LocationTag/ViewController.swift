24672f8c9b9f0485c32ce3020a7c237793a20be0
Mingu Chu <mingu@mingus-macbook-pro.local>
2016-05-27 12:36:05 -0500

    Initial Commit

---
+++ LocationTag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationTag
+//
+//  Created by Mingu Chu on 5/27/16.
+//  Copyright © 2016 Mingu Chu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
