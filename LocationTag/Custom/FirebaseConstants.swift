92cbe96f8e42134a3a596e69723d4ac7a18acffc
Mingu Chu <mingu@Mingus-MacBook-Pro.local>
2016-05-30 18:38:56 -0500

    added files

---
+++ LocationTag/Custom/FirebaseConstants.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  LocationTag
+//
+//  Created by Mingu Chu on 5/27/16.
+//  Copyright © 2016 Mingu Chu. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class FirebaseConstants {
+//    Initializers
+    static let firebase = FirebaseConstants()
+    
+    var ref:FIRDatabaseReference!
+//    ref = FIRDatabase.database().reference()
+
+    
+    
+    
+
+}
+
+
